cc4f7081938964d19601a9256a4f4d7cf26507de
mr <info@masujimaryohei.jp>
2020-05-10 21:55:18 +0900

    Add 33270326 project.

---
+++ Assets/Scripts/CameraRenderTexture.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class CameraRenderTexture : MonoBehaviour
+{
+    public Material Mat;
+
+    public float level;
+
+    public void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, Mat);
+    }
+
+    private void Update()
+    {
+        Mat.SetFloat("_RedX", Random.Range(-0.01f, 0.01f));
+        Mat.SetFloat("_RedY", Random.Range(-0.01f, 0.01f));
+        Mat.SetFloat("_GreenX", Random.Range(-0.01f, 0.01f));
+        Mat.SetFloat("_GreenY", Random.Range(-0.01f, 0.01f));
+        Mat.SetFloat("_BlueX", Random.Range(-0.01f, 0.01f));
+        Mat.SetFloat("_BlueY", Random.Range(-0.01f, 0.01f));
+    }
+}
---
+++ Assets/Scripts/Osaburo.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Osaburo : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource voicePass = default;
+    [SerializeField]
+    private AudioSource voiceIntro = default;
+
+    private int countPass = 0;
+    private int countIntro = 0;
+
+    private bool flagPass = false;
+    private bool flagIntro = false;
+
+    [SerializeField]
+    private Text textPass = default;
+
+    [SerializeField]
+    private CameraRenderTexture cameraRenderTexture = default;
+
+    public void SayPass()
+    {
+        voicePass.Play();
+        if (flagPass == false)
+        {
+            countPass++;
+        }
+        if (countPass > 20)
+        {
+            voicePass.pitch += 0.02f;
+            countPass = 0;
+            if (voicePass.pitch > 1.25f)
+            {
+                voicePass.pitch = 0.4f;
+                flagPass = true;
+            }
+        }
+    }
+
+    public void SayIntro()
+    {
+        voiceIntro.Play();
+        if (flagIntro == false)
+        {
+            countIntro++;
+        }
+        if (countIntro > 20)
+        {
+            voiceIntro.pitch += 0.02f;
+            countIntro = 0;
+            if (voiceIntro.pitch > 1.25f)
+            {
+                voiceIntro.pitch = 0.4f;
+                flagIntro = true;
+            }
+        }
+    }
+}
